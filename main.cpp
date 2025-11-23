23d15897b58c8ad5bbdd22323b20e402429dde9b
d0s1 <dominic1020@web.de>
2025-11-23 02:57:07 +0100

    added very basic operations to a calculator

--- main.cpp
+++ main.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-  cout << "Hello World!";
-  return 0;
+int calculator(){
+
+  double zahl1, zahl2;
+  char op;
+
+  cout << "Erste Zahl:";
+  cin >> zahl1;
+
+  cout << "Zweite Zahl:";
+  cin >> zahl2;
+
+  double ergebnis;
+  cout << "Operator";
+  cin >> op;
+
+  if (op == '+'){
+    ergebnis = zahl1 + zahl2;
+  }
+  else  if (op == '-') {
+    ergebnis = zahl1 - zahl2;
+  }
+  cout << "Ergebniss ist " << ergebnis;
+  return 1;
 }
+
+int main() {
+  return calculator();
+}
+
